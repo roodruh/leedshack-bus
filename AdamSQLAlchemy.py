6547469d0ac725a7f337e925fc2a44e29f0e2a6d
Dambuster888 <151151253+Dambuster888@users.noreply.github.com>
2025-02-08 18:53:13 +0000

    Pjson data commit

--- AdamSQLAlchemy.py
+++ AdamSQLAlchemy.py
@@ -1,7 +1,24 @@
+import json
+import xmltodict
+with open("siri.xml") as xml_file:
+    data_dict = xmltodict.parse(xml_file.read())
+
+json_data = json.dumps(data_dict)
+with open("data.json", "w") as json_file:
+        json_file.write(json_data)
 
 
 
 
+with open('data.json', 'r') as json_file:
+    json_object = json.load(json_file)
 
 
+
+#print(json.dumps(json_object))
+
+json_object = (json.dumps(json_object, indent=1))
+
+with open("Pdata.json", "w") as json_file:
+        json_file.write(json_data)
 
